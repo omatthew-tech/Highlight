a7d4e83b4537e2a8b3ee6b396377f9200c935978
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2023-06-24 11:22:28 -0500

    added badass background

--- posts/static/js/home.js
+++ posts/static/js/home.js
@@ -1,19 +1,44 @@
-window.onload = function() {
-    var boxes = document.querySelectorAll(".box");
+document.addEventListener('DOMContentLoaded', (event) => {
+    var rows = Array.from(document.querySelectorAll(".row"));
 
-    setInterval(function() {
-        // Reset the brightness for all boxes
-        boxes.forEach(function(box) {
-            box.style.filter = "";
-        });
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
-        var randomIndex = Math.floor(Math.random() * boxes.length);
-        var box = boxes[randomIndex];
-        box.style.filter = "brightness(1.0)";
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
 
-        setTimeout(function() {
-            box.style.filter = "";
-        }, 500);
-    }, 1000);
-};
+        return array;
+    }
+
+    rows = shuffle(rows);
+
+    function highlightRow(index) {
+        var row = rows[index];
+        row.classList.add("highlight");
+
+        // Check if there is another row to highlight
+        if (index < rows.length - 1) {
+            setTimeout(function() {
+                highlightRow(index + 1);
+            }, 2000); // Wait for the current row's animation to finish before starting the next one
+        }
+    }
+
+    highlightRow(0); // Start highlighting from the first row of the shuffled array
+});
+
+
+
+
+
+
+
+
+
+
 
