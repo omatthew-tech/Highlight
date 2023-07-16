cb3fa152607d5ca3a7e118241d99ddbeb6781734
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2023-07-15 19:49:24 -0500

    static

--- highlight/settings.py
+++ highlight/settings.py
@@ -130,7 +130,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
@@ -140,7 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 
 AWS_ACCESS_KEY_ID = 'AKIA2XVH2D65CNLVQRW6'
