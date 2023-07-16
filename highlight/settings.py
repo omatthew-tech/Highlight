f061d7dd714cbc0803286e8dae9414f6c2b078ef
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2023-07-15 20:21:44 -0500

    static

--- highlight/settings.py
+++ highlight/settings.py
@@ -128,7 +128,8 @@ AWS_LOCATION = 'static'
 if not DEBUG:
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEFAULT_FILE_STORAGE = 'highlight.storage_backends.MediaStorage'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIAFILES_LOCATION = 'media'
