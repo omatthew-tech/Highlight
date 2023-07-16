6d811ca4bc961d777f892e727c88651fec1e921c
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2023-07-16 12:59:15 -0500

    no debug setting

--- highlight/settings.py
+++ highlight/settings.py
@@ -125,7 +125,6 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 # Update STATIC_URL to use S3 storage when not in debug
-if not DEBUG:
     STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     
