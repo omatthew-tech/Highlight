6d50e8ec4abd3a712fd9d82bf7532947c3d2a6ed
omatthew-tech <72327693+omatthew-tech@users.noreply.github.com>
2023-07-15 19:37:48 -0500

    media files added to settings

--- highlight/settings.py
+++ highlight/settings.py
@@ -156,3 +156,6 @@ AWS_LOCATION = 'static'
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
